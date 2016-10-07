b963a35907295d6406c2c9006a95894fdb6ee505
Pedro Luis Valdivieso <pedro.valdivieso@gmail.com>
2016-10-06 22:02:48 -0400

    Initial Commit

---
+++ Kogi-Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Kogi-Test
+//
+//  Created by Pedro Luis Valdivieso on 10/6/16.
+//  Copyright © 2016 Pedro Luis Valdivieso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Kogi-Test/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Kogi-Test
+//
+//  Created by Pedro Luis Valdivieso on 10/6/16.
+//  Copyright © 2016 Pedro Luis Valdivieso. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
